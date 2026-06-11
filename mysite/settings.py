e15d1ab49e9ffac0b0c6477a049591dabf698265
himani <himanirouniyar15@gmail.com>
2026-06-11 21:53:15 +0545

    add cloudinary

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
 
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = 'myapp:index'
